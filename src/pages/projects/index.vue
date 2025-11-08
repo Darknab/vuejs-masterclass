22e6cd530585ea8d6846fa072ae3ae1b670680ca
Darknab <darknab@live.fr>
2025-11-08 20:23:00 +0100

    Add TypeScript support to Supabase

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { supabase } from '@/lib/supabaseClient'
 import { ref } from 'vue'
+import type { Tables } from '../../../database/types'
 
-const projects = ref()
+const projects = ref<Tables<'projects'>[] | null>(null)
 
 ;(async () => {
   const { data, error } = await supabase.from('projects').select()
@@ -19,6 +20,10 @@ const projects = ref()
   <div>
     <h1>Projects Page</h1>
     <RouterLink to="/">Go to home</RouterLink>
-    {{ projects }}
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project.name }}
+      </li>
+    </ul>
   </div>
 </template>
